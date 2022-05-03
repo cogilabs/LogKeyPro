3dce27b4ae77327a814791f161d2feb898cd41f3
manon.dauchy <dauchy.manon@hotmail.com>
2022-05-03 11:15:35 +0200

    Login avec firebase

---
+++ app/src/main/java/fr/logkey/logkeypro/Chambre/AllerVersChambreActivity.kt
@@ -0,0 +1,12 @@
+package fr.logkey.logkeypro.Chambre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.logkey.logkeypro.R
+
+class AllerVersChambreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aller_vers_chambre)
+    }
+}
--- app/src/main/java/fr/logkey/logkeypro/MainActivity.kt
+++ app/src/main/java/fr/logkey/logkeypro/MainActivity.kt
@@ -3,16 +3,19 @@ package fr.logkey.logkeypro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 import fr.logkey.logkeypro.Accueil.AccueilActivity
+import fr.logkey.logkeypro.ui.login.EmailPasswordActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         goToAccueil("")
     }
     private  fun goToAccueil(Accueil: String){
-        val intent = Intent(this, AccueilActivity::class.java)
+        val intent = Intent(this, EmailPasswordActivity::class.java)
         intent.putExtra("Acceuil", Accueil)
         startActivity(intent)
     }
